6bdc1f31fcb9326d66d2641f7350e0262c9c0e1d
cheolwook <loky123@naver.com>
2022-06-08 10:02:00 +0900

    x:Name은 이제부터 메서드 이름이 들어가게 된다

--- Day06/WpfCaliburnApp/WpfCaliburnApp/ViewModels/MainViewModel.cs
+++ Day06/WpfCaliburnApp/WpfCaliburnApp/ViewModels/MainViewModel.cs
@@ -1,8 +1,29 @@
 ﻿using Caliburn.Micro;
+using System.Windows;
 
 namespace WpfCaliburnApp.ViewModels
 {
     public class MainViewModel : Screen
     {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrEmpty(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show($"Hello~ {Name}");
+        }
     }
 }
